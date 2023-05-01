61a557e1dd0f95b24c4cbcbed58346926235e834
Joshua030 <Joseluy123>
2023-05-01 18:40:55 +0200

    Refactoring code to be more readable

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,32 @@
-import fs from 'fs';
+import fs from "fs";
 
-const matches = fs.readFileSync('football.csv',{
-  encoding: 'utf-8'
-}).split('\n').map(
-  (row:string):string[] => {
-    return row.split(',');
-  }
-);
+const matches = fs
+  .readFileSync("football.csv", {
+    encoding: "utf-8",
+  })
+  .split("\n")
+  .map((row: string): string[] => {
+    return row.split(",");
+  });
 
+enum MatchResult  {
+  homeWin= "H",
+  awayWin= "A",
+  draw= "D"
+};
+
+// const homeWin = "H";
+// const awayWin = "A";
+// const draw = "D";
 
 let manUnitedWins = 0;
 
-for(let match of matches ) {
-  if(match[1]==='Man United' && match[5] === 'H'){
+for (let match of matches) {
+  if (match[1] === "Man United" && match[5] === MatchResult.homeWin) {
     manUnitedWins++;
-  }else if (match[2]==='Man United' && match[5] === 'A'){
+  } else if (match[2] === "Man United" && match[5] === MatchResult.awayWin) {
     manUnitedWins++;
   }
-  };
+}
 
-  console.log(`Man United won ${manUnitedWins} games`);
-  
+console.log(`Man United won ${manUnitedWins} games`);

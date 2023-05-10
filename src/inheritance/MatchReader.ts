75b73dbaa32fc27c03ba5721f0c4bfba4573c880
Joshua030 <Joseluy123>
2023-05-10 18:08:09 +0200

    refactor code using composition

--- src/inheritance/MatchReader.ts
+++ src/inheritance/MatchReader.ts
@@ -1,22 +1,12 @@
+import { CsvFileReader } from "./CsvFileReader";
 import { MatchResult } from "./MatchResult";
 import { dateStringToDate } from "./utils";
 
-interface DataReader {
-  read(): void;
-  data: string[][];
-}
-
 type MatchData = [Date,string,string,number,number,MatchResult,string];
 
-export class MatchReader {
-
-  matches: MatchData[] = [];
-  constructor(public reader: DataReader) {}
-    load(): void{
-this.reader.read();
-this.matches = this.reader.data.map(
-(row:string[]):MatchData => {
-  return[
+export class MatchReader extends CsvFileReader<MatchData> {
+mapRow(row: string[]): MatchData {
+  return [
     dateStringToDate(row[0]),
     row[1],
     row[2],
@@ -24,9 +14,6 @@ this.matches = this.reader.data.map(
     parseInt(row[4]),
     row[5] as MatchResult,
     row[6]
-
-  ]
-});
-    }
-  
+  ];
+}
 }
